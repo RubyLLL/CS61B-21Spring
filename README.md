806b519fad6a722257e6a8e2b7456d1e9c29c908
RubyLLL <xlyu00green@gmail.com>
2023-01-12 21:15:23 +0800

    lab 1 finished

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cs61b 21 spring Data Structures
+> Skeleton code can be found [here]([here](https://github.com/Berkeley-CS61B/skeleton-sp21))
+>
+> 
---
+++ labs/lab1/Collatz.java
@@ -0,0 +1,27 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
+ */
+public class Collatz {
+
+    /** Collatz sequence */
+    public static int nextNumber(int n) {
+        /** prints out Collatz sequence starting at number n*/
+        if(n % 2 == 0){
+            return n / 2;
+        }
+        else{
+            return n * 3 + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+}
+
