26ef55f06d2750679e7a07dc4624b787f0cb15f8
RubyLLL <xlyu00green@gmail.com>
2023-01-18 23:19:48 +0800

    a better implementation of ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -10,37 +10,33 @@ public class ArrayDeque<T> {
 
     public ArrayDeque() {
         items = (T[]) new Object[8];
-        front = -1;
-        rear = 0;
+        front = 3;
+        rear = 4;
         size = 0;
     }
 
     public void addFirst(T item){
-        if (size == items.length){
-            resize(2 * items.length);
-        }
-        if(front == -1) front = items.length;
-        front--;
+        if (front == -1) resize(2 * items.length);
         items[front] = item;
-        size++;
+        front --;
+        size ++;
     }
 
     public void addLast(T item) {
-        if (size == items.length) {
-            resize(2 * items.length);
-        }
-        items[rear++] = item;
-        size++;
+        if(rear == items.length) resize(2 * items.length);
+        items[rear] = item;
+        rear ++;
+        size ++;
     }
 
     private void resize(int capacity) {
-        T[] res = (T[])new Object[capacity];
-        System.arraycopy(items, 0, res, 0, rear);
-        if(front != -1) {
-            System.arraycopy(items, front, res, capacity - (size - front), (size - front));
-            front = capacity-(size-front);
-        }
-        items = res;
+        T[] temp = (T[]) new Object[capacity];
+        int frontCount = items.length/2-1 - front;
+        int middle = capacity / 2;
+        System.arraycopy(items, front + 1, temp, middle - frontCount, size);
+        front = capacity/2 - (items.length / 2 - front);
+        rear = capacity/2 + (rear - items.length / 2);
+        items = temp;
     }
 
 
@@ -54,61 +50,36 @@ public class ArrayDeque<T> {
 
     public T removeFirst() {
         if(size == 0) return null;
-        if ((size < items.length / 4) && (size > 4)) {
-            resize(items.length / 4);
-        }
-        T first;
-        if(front == -1){
-            int index = 0;
-            while(items[index] == null) index ++;
-            first = items[index];
-            items[index] = null;
-        }
-        else{
-            first = items[front];
-            items[front] = null;
-            front++;
-        }
-        size--;
-        return first;
+        T target = items[front];
+        items[front] = null;
+        front ++;
+        size --;
+        return target;
     }
     public T removeLast() {
         if(size == 0) return null;
-        if ((size < items.length / 4) && (size > 4)) {
-            resize(items.length / 4);
-        }
-        return null;
+        T target = items[rear];
+        items[rear] = null;
+        rear --;
+        size --;
+        return target;
     }
 
     public T get(int index){
-        if(index >= size) return null;
-        if(front == -1) return items[index];
-        for(int i = front; i < items.length; i++){
-            if(index == 0) return items[i];
-            index--;
-        }
-        return items[index];
+        if(index >= size || index < -1) return null;
+        int target = front + 1 + index;
+        return items[target];
     }
 
     public void printDeque(){
-        String res = "";
         int curr = 0;
+        String res = "";
         while(curr < size){
-            res += get(curr++) + " ";
+            res += get(curr) + " ";
         }
         System.out.println(res);
     }
 
     public static void main(String[] args){
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        for(int i = 0; i < 100; i++){
-            deque.addLast(i);
-        }
-        int curr = 0;
-        while(curr < 100){
-            deque.removeFirst();
-            curr ++;
-        }
-
     }
 }
