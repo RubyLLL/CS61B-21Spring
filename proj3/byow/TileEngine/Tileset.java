027defb1a17726a3cf3c86b768c04d2b8a384604
RubyLLL <xlyu00green@gmail.com>
2023-02-06 18:54:35 +0800

    add rooms and walls, add custome tiles

--- proj3/byow/TileEngine/Tileset.java
+++ proj3/byow/TileEngine/Tileset.java
@@ -1,6 +1,7 @@
 package byow.TileEngine;
 
 import java.awt.Color;
+import java.io.File;
 
 /**
  * Contains constant tile objects, to avoid having to remake the same tiles in different parts of
@@ -17,13 +18,21 @@ import java.awt.Color;
  */
 
 public class Tileset {
+    public static final String CWD = System.getProperty("user.dir");
+    public static final String ASSETS = new File(CWD, "assets").toString();
     public static final TETile AVATAR = new TETile('@', Color.white, Color.black, "you");
     public static final TETile WALL = new TETile('#', new Color(216, 128, 128), Color.darkGray,
             "wall");
+    public static final TETile VWALL = new TETile('|', new Color(216, 128, 128), Color.darkGray,
+            "wall");
+    public static final TETile HWALL = new TETile('=', new Color(216, 128, 128), Color.darkGray,
+            "wall");
+    public static final TETile DWALL = new TETile('o', new Color(216, 128, 128), Color.darkGray,
+            "wall");
     public static final TETile FLOOR = new TETile('·', new Color(128, 192, 128), Color.black,
-            "floor");
+            "floor", ASSETS + "/inside.png");
     public static final TETile NOTHING = new TETile(' ', Color.black, Color.black, "nothing");
-    public static final TETile GRASS = new TETile('"', Color.green, Color.black, "grass");
+    public static final TETile GRASS = new TETile('"', Color.green, Color.black, "grass", ASSETS + "/grass.png");
     public static final TETile WATER = new TETile('≈', Color.blue, Color.black, "water");
     public static final TETile FLOWER = new TETile('❀', Color.magenta, Color.pink, "flower");
     public static final TETile LOCKED_DOOR = new TETile('█', Color.orange, Color.black,
